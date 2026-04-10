044ec63b6cfc8ba07033d9220d30e851158f4803
MrHatkeeper <krch.karelv@gmail.com>
2026-04-10 19:00:40 +0200

    Queens done

---
+++ knights empty.py
@@ -0,0 +1,134 @@
+# Solves the tasks with a knight on the chessboard:
+# 1) shortest path of a knight from the starting position on the chessboard to all other positions
+# 2) knight's tour, where the knight visits all squares of the chessboard exactly once
+
+class KnightSolution:
+    def __init__(self, n):
+        self.n = n  # size of the chessboard
+        ...
+        # self.board ... length of the path to each position from the start (-1 initially)
+        # self.predecessors ... predecessor of each position
+        # self.moves ... list of possible knight moves
+
+    def solve_shortest_paths(self, start):
+        """
+        Solves task 1: Finds the shortest path from the start position to all other squares using BFS.
+        Fills self.board with distances and self.predecessors with previous positions.
+        """
+        ...
+    
+    def solve_knight_tour(self, start, one_solution=True):
+        """
+        Solves task 2: Finds a knight's tour from the start position using DFS.
+        Updates self.board with move numbers and self.predecessors with the path.
+        If one_solution is True, stops after finding the first valid tour.
+        Prints all found solutions (matrices board and predecessors).
+        """
+        ...
+ 
+    def print_board(self):
+        """ Prints the current state of the board (distances or move numbers). """
+        ...
+
+    def print_predecessors(self):
+        """Prints the matrix of predecessors for each position"""
+        ...  
+
+    def get_distance(self, position):
+        """Returns the distance or move number at a given position"""
+        ...
+
+    def get_path(self, position):
+        """Reconstructs the path from the start to the given position using predecessors
+        Returns the list of states on the path from the start to the given position (inclusive)
+        """
+        ...
+
+# --- Testing functions ---
+
+def test_shortest_paths():
+    n = 5
+    knight_solution = KnightSolution(n)
+    knight_solution.solve_shortest_paths((0,0))
+
+    print("Distances from (0,0) to other positions:")
+    knight_solution.print_board()
+    print("\nPredecessors:")
+    knight_solution.print_predecessors()
+
+    end = (1, 1)
+    print(f"\nPath from (0,0) to {end}:")
+    print("Distance:", knight_solution.get_distance(end))
+    print("Path:", knight_solution.get_path(end))
+
+''' Solution:
+
+Distances from (0,0) to other positions:
+ 0  3  2  3  2
+ 3  4  1  2  3
+ 2  1  4  3  2
+ 3  2  3  2  3
+ 2  3  2  3  4
+
+ Predecessors: # may look different deppending on the ordering of moves
+(-1, -1) (1, 3) (2, 1) (2, 4) (1, 2)
+(0, 2) (2, 3) (0, 0) (2, 1) (0, 2)
+(1, 2) (0, 0) (3, 4) (4, 2) (1, 2)
+(4, 2) (1, 2) (4, 0) (2, 1) (4, 2)
+(2, 1) (3, 3) (2, 1) (3, 1) (2, 3)
+
+Path from (0,0) to (1,1):
+distance:  4
+path:  [(0, 0), (2, 1), (4, 2), (2, 3), (1, 1)]
+'''
+
+def test_tour():
+
+    n = 5
+    knight_solution = KnightSolution(n)
+ 
+    print("Path from (0,0) to other positions in the matrix:")
+    knight_solution.solve_knight_tour((0,0))
+
+    end = (4, 1)
+    print(f"\nPath from (0,0) to {end}:")
+    print("Move number:", knight_solution.get_distance(end))
+    print("Path:", knight_solution.get_path(end))
+
+''' One of possible solutions (you may get another one):
+
+Path from (0,0) to other positions in the matrix:
+ 0  5 14  9 20
+13  8 19  4 15
+18  1  6 21 10
+ 7 12 23 16  3
+24 17  2 11 22
+
+Predecessors: # may look different deppending on the ordering of moves
+(-1, -1) (1, 3) (1, 0) (1, 1) (1, 2)
+(3, 1) (3, 0) (2, 0) (3, 4) (0, 2)
+(4, 1) (0, 0) (0, 1) (0, 4) (0, 3)
+(2, 2) (4, 3) (4, 4) (1, 4) (4, 2)
+(3, 2) (3, 3) (2, 1) (2, 4) (2, 3)
+
+Path from (0,0) to (4,1):
+distance:  17
+path:  [(0, 0), (2, 1), (4, 2), (3, 4), (1, 3), (0, 1), (2, 2), (3, 0), (1, 1), (0, 3), (2, 4), (4, 3), (3, 1), (1, 0), (0, 2), (1, 4), (3, 3), (4, 1)]
+
+'''
+
+# --- Main entry point ---
+
+if __name__ == "__main__":  
+
+    test_shortest_paths()
+
+    print("\n" + "=" * 50 + "\n")
+    
+    test_tour()        
+    
+
+
+
+
+
---
+++ queens_empty.py
@@ -0,0 +1,174 @@
+# Solves the N-Queens problem:
+# Place N queens on an N×N chessboard so that no two queens attack each other.
+# Queens must not share the same row, column, or diagonal.
+import copy
+
+def is_safe(board, row, column):
+    """
+    Check whether a queen can be safely placed on board at position (row, column).
+
+    The position is considered unsafe if there is already a queen in the same row, column or on the upper or lower diagonal. 
+    
+    Parameters:
+        board (list of list): Current state of the chessboard.
+        row (int): Row index.
+        column (int): Column index.
+
+    Returns:
+        bool: True if the position is safe, False otherwise.
+    """
+    for j in range(column):
+        if board[row][j] == 1:
+            return False
+
+    i, j = row, column
+    while inBoundaries(board, i, j):
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    i, j = row, column
+    while inBoundaries(board, i, j):
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
+
+    return True
+
+def inBoundaries(board, row, column):
+    return 0 <= row < len(board) and 0 <= column < len(board[0])
+
+def print_board(board: list[list[int]]):
+    """
+    Print the current configuration of the chessboard.
+
+    Representation:
+        'X' → queen
+        '.' → empty cell
+
+    Each row is printed on a separate line.
+    """
+    out = ""
+
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == 0:
+                out += "."
+            else:
+                out += "X"
+        out += "\n"
+    print(out)
+
+def solve_n_queens(n, one_solution = True):
+    """
+    Solve the N-Queens problem using backtracking.
+
+    The algorithm incrementally builds the solution by placing queens
+    column by column and backtracking when a conflict occurs.
+
+    Parameters:
+        n (int): Size of the board (n×n) (and number of queens).
+        one_solution (bool):
+            - True  → stop after finding the first valid solution
+            - False → find and print all possible solutions
+
+    Returns:
+        int: Number of valid solutions found.
+    """
+    board =[]
+    for i in range(n):
+        board.append([])
+        for j in range(n):
+            board[i].append(0)
+    return placeQueen(board, 0, one_solution)
+
+
+def placeQueen(board, column, oneSolution) -> int:
+    if column == n:
+        print_board(board)
+        return 1
+
+    solutions = 0
+
+    for row in range(n):
+        if is_safe(board, row, column):
+            board[row][column] = 1
+            solutions += placeQueen(board, column + 1, oneSolution)
+            board[row][column] = 0
+            if oneSolution and solutions == 1:
+                return 1
+    return solutions
+# Example usage:
+if __name__ == "__main__":          
+    n = 5
+    number_of_solutions = solve_n_queens(n, False)
+    print(f"Total solutions found: {number_of_solutions}")
+
+'''
+Example output for n = 5 (all solutions):
+
+X . . . . 
+. . . X . 
+. X . . . 
+. . . . X
+. . X . .
+
+X . . . .
+. . X . .
+. . . . X
+. X . . .
+. . . X .
+
+. . X . .
+X . . . .
+. . . X .
+. X . . .
+. . . . X
+
+. . . X .
+X . . . .
+. . X . .
+. . . . X
+. X . . .
+
+. X . . .
+. . . X .
+X . . . .
+. . X . .
+. . . . X
+
+. . . . X
+. . X . .
+X . . . .
+. . . X .
+. X . . .
+
+. X . . .
+. . . . X
+. . X . .
+X . . . .
+. . . X .
+
+. . . . X
+. X . . .
+. . . X .
+X . . . .
+. . X . .
+
+. . . X .
+. X . . .
+. . . . X
+. . X . .
+X . . . .
+
+. . X . .
+. . . . X
+. X . . .
+. . . X .
+X . . . .
+
+Total solutions found: 10
+
+'''
